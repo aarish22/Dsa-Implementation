dc255ad0ddda9500d0d36d39245556b29429facc
aarish <aarishalam22@gmail.com>
2026-03-01 15:44:36 +0530

    added queue

---
+++ Queue/queue.py
@@ -0,0 +1,8 @@
+### front = rear = -1
+### first element is added rear = 0 and front = 0
+### second element is added rear = 1 and front = 0
+### insert => rear = (rear + 1) % size(to avoid overflow)
+### delete => front = (front + 1) % size(to avoid underflow)
+
+
+
