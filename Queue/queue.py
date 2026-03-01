5c1bdebd1e7baac397ffa65fbf9c8c1f1fed580e
aarish <aarishalam22@gmail.com>
2026-03-01 15:56:30 +0530

    added queue from queue branch

--- Queue/queue.py
+++ Queue/queue.py
@@ -3,6 +3,13 @@
 ### second element is added rear = 1 and front = 0
 ### insert => rear = (rear + 1) % size(to avoid overflow)
 ### delete => front = (front + 1) % size(to avoid underflow)
+### append <-- insert at rear
+## pop(0) <-- delete at front
 
 
+class Queue:
+  def __init__(self):
+    self.queue = []
+    
 
+  
