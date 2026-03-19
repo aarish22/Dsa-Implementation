c15aaa9e88f8ad1c3ae1f46e53f60409147fe847
aarish <aarishalam22@gmail.com>
2026-03-19 23:23:36 +0530

    add linked list desp

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -1,3 +1,5 @@
+## Linked List is a linear data structure where each element is a separate object. Each element (node) of a list is comprising of two items - the data and a reference to the next node. The last node has a reference to null. The entry point into a linked list is called the head of the list. It should be noted that head is not a separate node, but the reference to the first node. If the list is empty then the head is a null reference.
+
 class Node:
   def __init__ (self,data,next=None):
     self.data = data
