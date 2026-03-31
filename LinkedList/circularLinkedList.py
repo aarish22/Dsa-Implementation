cc57cab3e3da43410c637774080381bca172826b
aarish <aarishalam22@gmail.com>
2026-03-31 23:11:36 +0530

    add circular linkedlist implement

--- LinkedList/circularLinkedList.py
+++ LinkedList/circularLinkedList.py
@@ -1,3 +1,35 @@
 ## circular linked list implementation in python
 ## in circular linked list the last node points to the first node instead of null
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = self.head
+            return
+        current = self.head
+        while current.next != self.head:
+            current = current.next
+        current.next = new_node
+        new_node.next = self.head
+
+    def display(self):
+        if not self.head:
+            print("List is empty")
+            return
+        current = self.head
+        while True:
+            print(current.data, end=" ")
+            current = current.next
+            if current == self.head:
+                break
+        print()
